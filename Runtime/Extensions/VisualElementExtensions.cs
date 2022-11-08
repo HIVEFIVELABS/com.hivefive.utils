950b242f1d38158a525615b372ff7208868f60fa
Jiří Dušek <jirdus@gmail.com>
2022-11-08 11:03:59 +0100

    Reworked ResolveInlineMarkup

---
+++ Runtime/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using HiveFive.Utils;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+namespace Hivefive.Utils
+{
+    public static class VisualElementExtensions
+    {
+        public static T AddClass<T>(this T visualElement, string className) where T : VisualElement
+
+        {
+            Assert.IsNotNull(visualElement);
+            className.AssertIsNotNullOrEmpty();
+
+            visualElement.AddToClassList(className);
+            return visualElement;
+        }
+
+        public static T AddChild<T>(this T visualElement, VisualElement child) where T : VisualElement
+        {
+            Assert.IsNotNull(visualElement);
+            Assert.IsNotNull(child);
+
+            visualElement.Add(child);
+
+            return visualElement;
+        }
+
+        public static T AddChildren<T>(this T visualElement, IEnumerable<VisualElement> children)
+            where T : VisualElement
+        {
+            Assert.IsNotNull(visualElement);
+
+            foreach (var child in children) {
+                visualElement.AddChild(child);
+            }
+
+            return visualElement;
+        }
+    }
+}
---
+++ Runtime/Helpers/AssertUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Assertions;
+
+namespace HiveFive.Utils
+{
+    public static class AssertUtility
+    {
+        public static void AssertIsNotNullOrEmpty(this string s)
+        {
+            Assert.IsNotNull(s, "s != null");
+            Assert.IsTrue(s.Length > 0, "s.Length > 0");
+        }
+    }
+}
